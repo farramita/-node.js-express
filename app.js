2234e7051c533e1026d33d8dfd81a3002bcdc523
farramita <faramita9@gmail.com>
2022-06-07 15:59:27 +0700

    add all files
    hello world

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const chalk = require('chalk')
+const debug = require('debug')("app");
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'))
+
+app.get("/", (req,res)=>{
+
+    res.send("hello world from another world");
+
+})
+
+app.listen(port, ()=>{
+    debug("Listening on port %d" + chalk.green(" : "+port));
+})
